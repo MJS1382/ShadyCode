8b46c648c844e915b8cc1d234951854ab66b8824
Marshall Sadeghian <msadeghian262@gmail.com>
2026-09-10 04:01:32 +0330

    Added full crud for posts. Remove, View, Edit and publishing. fixed some bugs too.

--- wwwroot/js/main-layout.js
+++ wwwroot/js/main-layout.js
@@ -10,3 +10,12 @@ if (menuToggle && menu) {
 }
 
 document.getElementById("menu-toggle").addEventListener("click", () => console.log("FF"));
+
+window.hideBootstrapModal = (selector) => {
+    const element = document.querySelector(selector);
+
+    if (!element) return;
+
+    const modal = bootstrap.Modal.getOrCreateInstance(element);
+    modal.hide();
+};
